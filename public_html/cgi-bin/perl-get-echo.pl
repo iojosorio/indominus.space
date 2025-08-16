ba89f0edc4a7328546d6924f4d0578fb8c621320
iii <jonathannoj0186@gmail.com>
2025-08-15 17:35:21 -0700

    Included the Perl files. Part 0.

---
+++ public_html/cgi-bin/perl-get-echo.pl
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html><head><title>GET Request Echo</title>
+</head><body><h1 align="center">GET Request Echo</h1>
+<hr>
+<p><b/>Raw Query String:</b> </p>
+<p>Formatted Query String:</p>
+<ul>
+</ul></body></html>
