a3b5ac9b42debdb0c48675a79191bbcfe9c1ca11
Your Name <your.email@example.com>
2025-08-18 01:19:03 +0000

    Only sessioning is needed now. Part 1.

--- public_html/cgi-bin/python-get-echo.py
+++ public_html/cgi-bin/python-get-echo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import os
-import cgi
+import urllib.parse
 
 # Print HTTP headers
 print("Cache-Control: no-cache")
 print("Content-type: text/html\n")
 
-# Print HTML header
+# HTML header
 print("<html><head><title>GET query string</title></head>")
 print("<body><h1 align=center>GET query string</h1><hr/>")
 
@@ -14,13 +14,15 @@ print("<body><h1 align=center>GET query string</h1><hr/>")
 query_string = os.environ.get("QUERY_STRING", "")
 print(f"Raw query string: {query_string}<br/><br/>")
 
-# Parse query string
+# Parse query string manually
 print("<table> Formatted Query String:")
-form = cgi.FieldStorage()
-for key in form.keys():
-    value = form.getvalue(key)
-    print(f"<tr><td>{key}:</td><td>{value}</td></tr>")
+if query_string:
+    params = urllib.parse.parse_qs(query_string)
+    for key, values in params.items():
+        for value in values:
+            print(f"<tr><td>{key}:</td><td>{value}</td></tr>")
 print("</table>")
 
-# Print HTML footer
+# HTML footer
 print("</body></html>")
+
