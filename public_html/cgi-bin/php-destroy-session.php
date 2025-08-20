fa52be900074027f2de3c8ef354d19ab41ea0dfa
iii <jonathannoj0186@gmail.com>
2025-08-19 18:06:44 -0700

    Updated some links. Part 1.

--- public_html/cgi-bin/php-destroy-session.php
+++ public_html/cgi-bin/php-destroy-session.php
@@ -12,8 +12,8 @@ setcookie(session_name(), '', time() - 3600); // Remove session cookie
 </head>
 <body>
     <h1>Session Destroyed</h1>
-    <a href="php-cgiform.html">Back to PHP Form</a><br/>
-    <a href="php-sessions-1.php">Back to Page 1</a><br/>
-    <a href="php-sessions-2.php">Back to Page 2</a>
+    <a href="/php-cgiform.html">Back to PHP Form</a><br/>
+    <a href="/cgi-bin/php-sessions-1.php">Back to Page 1</a><br/>
+    <a href="/cgi-bin/php-sessions-2.php">Back to Page 2</a>
 </body>
 </html>
--- public_html/cgi-bin/python-sessions-1.py
+++ public_html/cgi-bin/python-sessions-1.py
@@ -21,11 +21,10 @@ def save_session(session_id, session_data):
         pickle.dump(session_data, f)
 
 # Read cookies
-cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
-session_id = cookie.get("CGISESSID")
-
-if session_id:
-    session_id = session_id.value
+cookie_header = os.environ.get("HTTP_COOKIE", "")
+cookie = http.cookies.SimpleCookie(cookie_header)
+if "CGISESSID" in cookie:
+    session_id = cookie["CGISESSID"].value
 else:
     session_id = str(uuid.uuid4())
 
@@ -37,7 +36,7 @@ if os.environ.get("REQUEST_METHOD", "") == "POST":
     length = int(os.environ.get("CONTENT_LENGTH", 0))
     post_data = sys.stdin.read(length)
     form = parse_qs(post_data)
-else:  # GET
+else:
     query = os.environ.get("QUERY_STRING", "")
     form = parse_qs(query)
 
@@ -49,28 +48,25 @@ if name:
 # Save session
 save_session(session_id, session)
 
-# Headers
+# Send headers (with path!)
 print("Content-Type: text/html")
-print(f"Set-Cookie: CGISESSID={session_id}")
+print(f"Set-Cookie: CGISESSID={session_id}; Path=/cgi-bin/")  # important!
 print()
 
-# HTML Output
+# HTML output
 print("<html>")
 print("<head><title>Python Sessions</title></head>")
 print("<body>")
 print("<h1>Python Sessions Page 1</h1>")
-
 if name:
     print(f"<p><b>Name:</b> {name}</p>")
 else:
     print("<p><b>Name:</b> You do not have a name set</p>")
 
-print("<br/><br/>")
+print('<br/><br/>')
 print('<a href="/cgi-bin/python-sessions-2.py">Session Page 2</a><br/>')
 print('<a href="/python-cgiform.html">Python CGI Form</a><br/>')
-
 print('<form style="margin-top:30px" action="/cgi-bin/python-destroy-session.py" method="get">')
 print('<button type="submit">Destroy Session</button>')
 print('</form>')
-
 print("</body></html>")
